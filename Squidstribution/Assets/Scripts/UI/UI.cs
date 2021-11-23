dc408c2ba5a513805e842286ddfbb1b85ad0b34b
SeriTheButterfly <71879145+SeriTheButterfly@users.noreply.github.com>
2021-11-23 22:42:29 +0000

    made the UI work with GameManager

--- Squidstribution/Assets/Scripts/UI/UI.cs
+++ Squidstribution/Assets/Scripts/UI/UI.cs
@@ -7,26 +7,29 @@ public class UI : MonoBehaviour
 {
     [SerializeField] private Text destructionText, karmaText;
     [SerializeField] private Slider healthSlider, threatSlider;
-    [SerializeField] private GameObject pausePanel, menuButton;
+    [SerializeField] private GameObject pausePanel, menuButton, gameManager;
 
     private GameObject player;
-    [SerializeField] private int playerHealth, fullHealth, threat, karma, destruction;
+    [SerializeField] private int playerHealth, fullHealth;
     private bool paused;
+    private GameManagerScript script;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         healthSlider.maxValue = fullHealth;
         pausePanel.SetActive(false);
+        script = gameManager.GetComponent<GameManagerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         healthSlider.value = playerHealth;
-        threatSlider.value = threat;
-        karmaText.text = "Karma: " + karma;
-        destructionText.text = "Destruction: " + destruction + "%";
+        threatSlider.value = script.GetThreat();
+        karmaText.text = "Karma: " + script.GetKarma().ToString();
+        destructionText.text = "Destruction: " + script.GetDestruction().ToString() + "%";
     }
 
     public void Menu()
