4422515773293f596cba0b3aac3ab53294cf1054
Ahillman2000 <55785328+Ahillman2000@users.noreply.github.com>
2021-11-24 10:56:02 +0000

    post lecture push

--- Squidstribution/Assets/Scripts/Player/ClickToScreen.cs
+++ Squidstribution/Assets/Scripts/Player/ClickToScreen.cs
@@ -15,55 +15,50 @@ public class ClickToScreen : MonoBehaviour
 
     void Start()
     {
-        anim = player.GetComponent<Animator>();
+        anim  = player.GetComponent<Animator>();
         agent = player.GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //create a ray cast and set it to the mouses cursor position in game
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, float.PositiveInfinity))
             {
                 GameObject hitObject = hit.transform.gameObject;
-                //Debug.Log(hitObject.tag);
 
-                // Player can move to a space on this object (road etc.)
+                Vector3 newTargetPos = hit.point;
+                agent.SetDestination(newTargetPos);
+                /// if cannot reach target position then stay at current position
 
-                    Vector3 newTargetPos = hit.point;
-                    agent.SetDestination(newTargetPos);
-                
-
-                // Player can damage this object (intact building ect)
                 if (hitObject.CompareTag("Damagable"))
                 {
-                    //need to organise this, we want him to move to a location on click, but animation stuff should be outside and I need to check if he's at the hitObject to start the animation. Will fix after sleep
-                    if (Vector3.Distance(agent.gameObject.transform.position, newTargetPos) == 0)
+                    if (Vector3.Distance(player.transform.position, newTargetPos) <= 10f)
                     {
                         anim.SetInteger("AttackIndex", Random.Range(0, 3));
                         anim.SetTrigger("Attack");
                         Building buildingScript = hitObject.GetComponent<Building>();
                         buildingScript.TakeDamage(100);
                     }
-                    
                 }
 
-                // Player can destroy this object (pieces of buildings)
-                if (hitObject.CompareTag("Destructable"))
+                if (hitObject.CompareTag("Destructable") && agent.remainingDistance <= 5f)
                 {
                     Rigidbody hitObjectRB = hitObject.GetComponent<Rigidbody>();
-                    hitObjectRB.AddExplosionForce(explosionForce, hit.point, explosionRadius, upwardsModifier, ForceMode.Impulse);
+                    hitObjectRB.AddExplosionForce(explosionForce, player.transform.position, explosionRadius, upwardsModifier, ForceMode.Impulse);
                 }
 
-                // player can attack this object (enemy)
                 if (hitObject.CompareTag("Attackable"))
                 {
                     
+                }
+
+                if (hitObject.CompareTag("Nestable"))
+                {
+
                 }
             }
         }
