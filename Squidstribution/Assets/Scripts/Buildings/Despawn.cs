27de850bd31974dbf066c8814b843d7c651497f3
William <Hamish2.Lawrence@live.uwe.ac.uk>
2021-11-27 12:50:38 +0000

    added a couple more buildings

--- Squidstribution/Assets/Scripts/Buildings/Despawn.cs
+++ Squidstribution/Assets/Scripts/Buildings/Despawn.cs
@@ -11,7 +11,7 @@ public class Despawn : MonoBehaviour
     // Update is called once per frame
     IEnumerator DespawnTime()
     {
-        float rand = Random.Range(3.5f, 8.4f);
+        float rand = Random.Range(6.6f, 14.1f);
         yield return new WaitForSeconds(rand);
         Destroy(this.gameObject);
     }
