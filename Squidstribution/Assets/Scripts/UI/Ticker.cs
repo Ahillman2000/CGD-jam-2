102c6e8388e25d11cc222f763b334bf79456368d
SeriTheButterfly <71879145+SeriTheButterfly@users.noreply.github.com>
2021-11-28 17:00:03 +0000

    Added news scrolling

---
+++ Squidstribution/Assets/Scripts/UI/Ticker.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ticker : MonoBehaviour
+{
+    [SerializeField] private TickerText tickerTextPrefab;
+    [Range(1f, 10f)] [SerializeField] private float itemDuration = 3f;
+    [SerializeField] private string[] fillerItems;
+
+    private float width;
+    private float pixelsPerSecond;
+    TickerText currentText;
+
+    private bool newsing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = GetComponent<RectTransform>().rect.width;
+        pixelsPerSecond = width / itemDuration;
+        StartCoroutine(DelayNews());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!newsing)
+        {
+            if (currentText.GetXPosition <= -currentText.GetWidth)
+            {
+                StartCoroutine(DelayNews());
+            }
+        }
+    }
+
+    void AddTickerText(string message)
+    {
+        currentText = Instantiate(tickerTextPrefab, transform);
+        currentText.Initialize(width, pixelsPerSecond, message);
+    }
+
+    IEnumerator DelayNews()
+    {
+        newsing = true;
+        yield return new WaitForSeconds(0.5f);
+        AddTickerText(fillerItems[Random.Range(0, fillerItems.Length)]);
+        newsing = false;
+    }
+}
---
+++ Squidstribution/Assets/Scripts/UI/TickerText.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TickerText : MonoBehaviour
+{
+    float tickerWidth, pixelsPerSecond;
+    RectTransform rt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.position += Vector3.left * pixelsPerSecond * Time.deltaTime;
+        if (GetXPosition <= 0 - tickerWidth - GetWidth)
+        {   
+            Destroy(gameObject);
+        }
+    }
+
+    public float GetXPosition { get { return rt.anchoredPosition.x; } }
+    public float GetWidth { get { return rt.rect.width; } }
+    public void Initialize(float tickerWidth, float pixelsPerSecond, string message)
+    {
+        this.tickerWidth = tickerWidth;
+        this.pixelsPerSecond = pixelsPerSecond;
+        rt = GetComponent<RectTransform>();
+        GetComponent<TextMeshProUGUI>().text = message;
+    }
+}
