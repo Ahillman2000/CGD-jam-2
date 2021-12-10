d592fb15f044941ec3b5bcf376d599fc08ec1817
William <Hamish2.Lawrence@live.uwe.ac.uk>
2021-12-10 17:12:01 +0000

    fixed pause menu issue

--- Squidstribution/Assets/Scripts/Player/ClickToScreen.cs
+++ Squidstribution/Assets/Scripts/Player/ClickToScreen.cs
@@ -24,79 +24,83 @@ public class ClickToScreen : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !uiScript.paused && !uiScript.onMenuButton)
+        if (!uiScript.paused && !uiScript.onMenuButton)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, float.PositiveInfinity, Clickable))
+            if (Input.GetMouseButtonDown(0))
             {
-                GameObject hitObject = hit.transform.gameObject;
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    SquidSelect.Instance.ShiftSelect(hitObject);
-                }
-                else
-                {
-                    SquidSelect.Instance.ClickSelect(hitObject);
-                }
-            }
-            else if(Physics.Raycast(ray, out hit, float.PositiveInfinity, Ground) && !Input.GetKey(KeyCode.LeftControl))
-            {
-                GameObject hitObject = hit.transform.gameObject;
-                //Debug.Log(hitObject.name);
-                uiScript.SettargetObject(hitObject);
-                Vector3 newTargetPos = hit.point;
-                agent.SetDestination(newTargetPos);
-                if(!Input.GetKey(KeyCode.LeftShift))
-                {
-                    SquidSelect.Instance.DeselectAll();
-                }
-                
-            }
-            else if (Physics.Raycast(ray, out hit, float.PositiveInfinity, Ground) && Input.GetKey(KeyCode.LeftControl))
-            {
-                Transform newTarget = hit.transform;
-                foreach(GameObject baby_agent in SquidSelect.Instance.SquidsSelected)
-                {
-                    baby_agent.GetComponent<BabySquid>().SetTarget(newTarget);
-                }
-            }
-        }
-        if (Input.GetMouseButtonDown(1) && !currentlyAttacking)
-        {
-            currentlyAttacking = true;
-            anim.SetInteger("AttackIndex", 1/*Random.Range(0, 3)*/); /// 1 = sweep attack 
-            anim.SetTrigger("Attack");
-            clipLength = anim.GetCurrentAnimatorStateInfo(0).length / 6 /*anim.GetCurrentAnimatorStateInfo(0).speed*/;  /// 6 is the animation speed of sweep attack 
-            StartCoroutine(WaitForAnimationToAttack(clipLength));
-            //BABY SQUIDS TO BE ABLE TO ATTACK TARGET SET HERE
-        }
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-        if (Input.GetKey(KeyCode.Return))
-        {
-            foreach (GameObject baby_agent in SquidSelect.Instance.SquidsSelected)
-            {
-                baby_agent.GetComponent<BabySquid>().SetTarget(player.transform);
-            }
-            SquidSelect.Instance.DeselectAll();
-        }
+                if (Physics.Raycast(ray, out hit, float.PositiveInfinity, Clickable))
+                {
+                    GameObject hitObject = hit.transform.gameObject;
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        SquidSelect.Instance.ShiftSelect(hitObject);
+                    }
+                    else
+                    {
+                        SquidSelect.Instance.ClickSelect(hitObject);
+                    }
+                }
+                else if (Physics.Raycast(ray, out hit, float.PositiveInfinity, Ground) && !Input.GetKey(KeyCode.LeftControl))
+                {
+                    GameObject hitObject = hit.transform.gameObject;
+                    //Debug.Log(hitObject.name);
+                    uiScript.SettargetObject(hitObject);
+                    Vector3 newTargetPos = hit.point;
+                    agent.SetDestination(newTargetPos);
+                    if (!Input.GetKey(KeyCode.LeftShift))
+                    {
+                        SquidSelect.Instance.DeselectAll();
+                    }
 
-        if (agent.velocity != Vector3.zero)
-        {
-            anim.SetBool("IsMoving", true);
+                }
+                else if (Physics.Raycast(ray, out hit, float.PositiveInfinity, Ground) && Input.GetKey(KeyCode.LeftControl))
+                {
+                    Transform newTarget = hit.transform;
+                    foreach (GameObject baby_agent in SquidSelect.Instance.SquidsSelected)
+                    {
+                        baby_agent.GetComponent<BabySquid>().SetTarget(newTarget);
+                    }
+                }
+            }
+            if (Input.GetMouseButtonDown(1) && !currentlyAttacking)
+            {
+                currentlyAttacking = true;
+                anim.SetInteger("AttackIndex", 1/*Random.Range(0, 3)*/); /// 1 = sweep attack 
+                anim.SetTrigger("Attack");
+                clipLength = anim.GetCurrentAnimatorStateInfo(0).length / 6 /*anim.GetCurrentAnimatorStateInfo(0).speed*/;  /// 6 is the animation speed of sweep attack 
+                StartCoroutine(WaitForAnimationToAttack(clipLength));
+                //BABY SQUIDS TO BE ABLE TO ATTACK TARGET SET HERE
+            }
+
+            if (Input.GetKey(KeyCode.Return))
+            {
+                foreach (GameObject baby_agent in SquidSelect.Instance.SquidsSelected)
+                {
+                    baby_agent.GetComponent<BabySquid>().SetTarget(player.transform);
+                }
+                SquidSelect.Instance.DeselectAll();
+            }
+
+            if (agent.velocity != Vector3.zero)
+            {
+                anim.SetBool("IsMoving", true);
+            }
+            else
+            {
+                anim.SetBool("IsMoving", false);
+                player.GetComponent<Rigidbody>().isKinematic = true;
+                player.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            if (Time.timeScale < 1)
+            {
+                Time.timeScale += 0.001f;
+            }
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
         }
-        else
-        {
-            anim.SetBool("IsMoving", false);
-            player.GetComponent<Rigidbody>().isKinematic = true;
-            player.GetComponent<Rigidbody>().isKinematic = false;
-        }
-        if(Time.timeScale < 1)
-        {
-            Time.timeScale += 0.001f;
-        }
-        Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
     private IEnumerator WaitForAnimationToAttack(float time)
     {
